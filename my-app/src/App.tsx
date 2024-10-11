360fe87098575bc30335abe9f62a90d508dea2a7
zlzcty <zlzcty@example.com>
2024-10-10 23:04:59 -0700

    Exercise 4.3.1 'Create' Done

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -21,17 +21,71 @@ function App() {
         });
     };
 
+    // 4.3.1 Create
+
+    const [notes, setNotes] = useState(dummyNotesList);
+    const initialNote = {
+        id: -1,
+        title: '',
+        content: '',
+        label: Label.other,
+    };
+    const [createNote, setCreateNote] = useState(initialNote);
+
+    const createNoteHandler = (event: React.FormEvent) => {
+        event.preventDefault();
+        console.log('title: ', createNote.title);
+        console.log('content: ', createNote.content);
+        createNote.id = notes.length + 1;
+        setNotes([createNote, ...notes]);
+        setCreateNote(initialNote);
+    };
+
     return (
         <ThemeProvider>
             <div className="app-container">
                 <div>
-                    <form className="note-form">
+                    <form className="note-form" onSubmit={createNoteHandler}>
                         <div>
-                            <input placeholder="Note Title"></input>
+                            <input
+                                placeholder="Note Title"
+                                onChange={(event) =>
+                                    setCreateNote({
+                                        ...createNote,
+                                        title: event.target.value,
+                                    })
+                                }
+                                required
+                            ></input>
                         </div>
 
                         <div>
-                            <textarea></textarea>
+                            <textarea
+                                onChange={(event) =>
+                                    setCreateNote({
+                                        ...createNote,
+                                        content: event.target.value,
+                                    })
+                                }
+                                required
+                            ></textarea>
+                        </div>
+
+                        <div>
+                            <select
+                                onChange={(event) =>
+                                    setCreateNote({
+                                        ...createNote,
+                                        label: event.target.value as Label,
+                                    })
+                                }
+                                required
+                            >
+                                <option value={Label.personal}>Personal</option>
+                                <option value={Label.study}>Study</option>
+                                <option value={Label.work}>Work</option>
+                                <option value={Label.other}>Other</option>
+                            </select>
                         </div>
 
                         <div>
@@ -48,7 +102,7 @@ function App() {
                         ))}
                     </ul>
                 </div>
-
+                {/* 
                 <div className="notes-grid">
                     {dummyNotesList.map((note) => (
                         <div key={note.id} className="note-item">
@@ -64,7 +118,25 @@ function App() {
                             <p> {note.label} </p>
                         </div>
                     ))}
+                </div> */}
+
+                <div className="notes-grid">
+                    {notes.map((note) => (
+                        <div key={note.id} className="note-item">
+                            <div className="notes-header">
+                                <FavoriteButton
+                                    toggleFavorite={toggleFavorite}
+                                    note={note}
+                                />
+                                <button>x</button>
+                            </div>
+                            <h2> {note.title} </h2>
+                            <p> {note.content} </p>
+                            <p> {note.label} </p>
+                        </div>
+                    ))}
                 </div>
+
                 {/* <ClickCounter /> */}
             </div>
         </ThemeProvider>
